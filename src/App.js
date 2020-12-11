b46170fff8c9e56a9cca1a01da60cb54092435ee
Daniele Mariotto <daniele.mariotto@gmail.com>
2020-12-11 17:05:24 +0100

    setted the getAir with params

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import {getCityAir} from './utils/api/api.js'
+import {getAir} from './utils/api/api.js'
 
 function App() {
 
 
-  const getTest = () =>{
-     getCityAir('Brescia', 'Lombardy')
+  const getTest = (citta) =>{
+    console.log('citta.city', citta[0].city) //test
+    //  getAir(citta[0].city, citta[0].region)
+    getAir('specific-city', 'Lombardy','Brescia') //test: get info about a city
     
   }
+  
+const citta = { //test object
+  0:{
+      'city':'Rome',
+      'region': 'Lazio'
+  }
+}
   return (
     <div className="App">
-<button onClick={()=> getTest()}>get meteo data of brescia</button>
+<button onClick={()=> getTest(citta)}>get meteo data of brescia</button>
     </div>
   );
 }
