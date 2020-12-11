aeb3f97dc2209fca4217e7e780ae75d9fa58b29e
Daniele Mariotto <daniele.mariotto@gmail.com>
2020-12-11 17:05:48 +0100

    setted switch and parameters for url

--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -2,20 +2,20 @@ import { AIR_URL, API_KEY } from "../../constant";
 
 const chooseUrl = (what, region, city ) =>{
   let url = "";
-  // ORIGINALE:  const url = `${AIR_URL}/city?city=${city}&state=${region}&country=Italy&key=${API_KEY}`
+
   switch (what) {
   case "region": {
 
-     url = `${AIR_URL}/states?country=Italy&key=${API_KEY}`;
+     url = `${AIR_URL}/states?country=Italy&key=${API_KEY}`; // get a list of supported regions
      break
   }
   case "list-cities": {
 
-     url = `${AIR_URL}/cities?state=${region}&country=Italy&key=${API_KEY}`;
+     url = `${AIR_URL}/cities?state=${region}&country=Italy&key=${API_KEY}`; //get a list of cities supported for a specific region
      break
   }
   case "specific-city": {
-     url = `${AIR_URL}/city?city=${city}&state=${region}&country=Italy&key=${API_KEY}`;
+     url = `${AIR_URL}/city?city=${city}&state=${region}&country=Italy&key=${API_KEY}`; // get info for a specific scity
      break
   }
   default: 
