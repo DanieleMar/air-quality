ed70d63018be2e04b2f657d4c640857647014982
Dan <daniele.mariotto@gmail.com>
2020-12-11 11:51:47 +0100

    get brescia data in clg when click button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import {getCityAir} from './utils/api/api.js'
 
 function App() {
+
+
+  const getTest = () =>{
+     getCityAir('Brescia', 'Lombardy')
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<button onClick={()=> getTest()}>get meteo data of brescia</button>
     </div>
   );
 }
