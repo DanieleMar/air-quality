9ee6edfaffe16c0aa3d7b49e135294eee55af4d6
Daniele Mariotto <daniele.mariotto@gmail.com>
2020-12-12 18:33:11 +0100

    render the app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-import {getAir} from './utils/api/api.js'
+import logo from "./logo.svg";
+import "./App.css";
+import { getAir } from "./utils/api/api.js";
+import ListItem from "./components/ListItem";
+import { useEffect, useState } from "react";
+
 
 function App() {
+  const [regions, setRegions] = useState([]);
+  const [cities, setCities] = useState([]);
 
-
-  const getTest = (citta) =>{
-    console.log('citta.city', citta[0].city) //test
-    //  getAir(citta[0].city, citta[0].region)
-    getAir('specific-city', 'Lombardy','Brescia') //test: get info about a city
+  const getData = async (what, region, city) => { //fetch data
+    const data = await getAir(what, region, city);
+    if (data !== undefined) {
+      
+      if (what==='regions') {
+        setRegions(data.data);
+      }
+      else if (what==='list-cities') {
+        console.log('list-cities data', data);
+        setCities(data.data)
+      }
+      
     
-  }
-  
-const citta = { //test object
-  0:{
-      'city':'Rome',
-      'region': 'Lazio'
-  }
-}
+    }
+  };
+
+
+
+  const fetchNextList = async (nextThingToExplore) => { //show the cities of a specific region
+    const newDatas = await getData("list-cities", nextThingToExplore);
+
+    setCities(newDatas)
+    setRegions({});
+  };
+
   return (
     <div className="App">
-<button onClick={()=> getTest(citta)}>get meteo data of brescia</button>
+      <button onClick={() => getData("regions")}>get list of Regions</button>
+
+
+      {cities !== undefined && cities.length > 0 //showed list of cities after the list of regions
+        ? cities.map((el, index) => {
+         
+            return (
+              <ListItem 
+                key={index}
+                element={el.city}
+                // openRegion={() => fetchNextList(el.city)}
+              />
+            );
+          })
+        : regions !== undefined && regions.length > 0
+        ? regions.map((el, index) => {
+            return (
+              <ListItem
+                key={index}
+                element={el.state}
+                openRegion={() => fetchNextList(el.state)}
+              />
+            );
+          })
+        : null}
     </div>
   );
 }
