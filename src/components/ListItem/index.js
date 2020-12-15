2e463c5ceb8add6b8dded816787e122883341cd0
Daniele Mariotto <daniele.mariotto@gmail.com>
2020-12-15 10:11:15 +0100

    added conditional simbol between words

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 
+
 export default function ListItem(props) {
 
-  const { element, openRegion } = props;
-  return <div onClick={openRegion}>{element}</div>;
+  const { element, openRegion, len, num} = props;
+  return <> <span onClick={openRegion}>{element}</span> {num<len-1 && <span>&diams;</span>}</>;
 }
